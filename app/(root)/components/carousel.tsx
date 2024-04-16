ff024a2c890efb4a41b165a870e75e8b07808426
3111TAN-13 <3111TAN-13@boronkay.local>
2024-04-16 11:03:30 +0200

    landing page

--- app/(root)/components/carousel.tsx
+++ app/(root)/components/carousel.tsx
@@ -6,37 +6,52 @@ import Image from 'next/image';
 import { motion } from "framer-motion"
 import { Rating } from '@mui/material'
 import { Carrot, Eye, ShoppingCart } from 'lucide-react';
+import { Products } from '@prisma/client';
+import Link from 'next/link';
 
+interface CarouselProps {
+  products: Products[]
+}
 
-export default function CarouselSlider() {
+const CarouselSlider: React.FC<CarouselProps> = ({ products }) => {
   return (
     <motion.div className="px-8 mt-6 flex flex-col items-center bg-gradient-to-b " initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}>
-   
+
       <div className="grid gap-6 mt-12 grid-cols-1 md:grid-cols-3 xl:grid-cols-5">
-        {examples.map((product, i) => (
-          <div className="border bg-white transition hover:scale-105 shadow-black border-black/20 rounded-xl p-2 relative flex flex-col justify-between max-w-[370px]" key={i}>
-            <div>
-              <div className='flex justify-center items-center h-[17rem] mb-4 object-contain overflow-hidden'>
-                <Image alt="xd" width={300} height={300} src={product.imageUrl} className="aspect-square w-[75%] mix-blend-multiply" />
+        {products.map((product, i) => (
+          <Link href={`market/${product.id}`} key={product.id}>
+            <div
+              className="w-[17rem] border from-white to bg-yellow-50 transition hover:scale-105 bg-gradient-to-t shadow-black border-black/20 rounded-xl p-2 relative flex flex-col justify-between"
+              key={product.id}
+            >
+              <div>
+                <div className="flex justify-center items-center h-[17rem] mb-4 object-contain overflow-hidden">
+                  <Image
+                    alt="xd"
+                    width={300}
+                    height={300}
+                    src={product.imageUrl}
+                    className="aspect-square w-[17rem] mix-blend-multiply rounded-md bg-red-400"
+                  />
+                </div>
+                <div className="text-xl font-semibold">{product.label}</div>
               </div>
-              <div className="relative h-[6rem]">
-              <div>{product.name}</div>
-            </div>
-            <div className="absolute bottom-8">
-              <p>{product.price}</p>
-            </div>
+              <div>
+                <p>{product.price} Ft</p>
               </div>
-              
-            <div className="h-[2rem]"></div>
-            <button className="bg-yellow-400 h-[2rem] w-full absolute bottom-0 left-[50%] translate-x-[-50%] rounded-b-xl px-4 items-center flex justify-center text-md gap-2">
-          <p>Megtekintem</p>
-          <Eye />
-        </button>
-          </div>
+              <div className="h-[2rem]"></div>
+              <button className="bg-yellow-400 h-[2rem] w-full absolute bottom-0 left-[50%] translate-x-[-50%] rounded-b-xl px-4 items-center flex justify-center text-md gap-2">
+                <p>Megtekintem</p>
+                <Eye />
+              </button>
+            </div>
+          </Link>
         ))}
 
       </div>
     </motion.div>
   )
 }
+
+export default CarouselSlider
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,15 +5,24 @@ import Header from "./components/header";
 import Separator from "./components/separator";
 import Qualities from "./components/qualities";
 import AboutSection from "./components/about";
+import prismadb from "@/lib/prismadb";
+import { Products } from "@prisma/client";
 
-export default function Home() {
+export default async function Home() {
+  const products = await prismadb.products.findMany({
+    where: {
+      storeId: 'aa54d763-6f53-4a3c-a192-f515a2de950f'
+    },
+    take: 5
+  });
+  console.log()
   return (
     <div className="flex flex-col items-center">
       <Header />
-      <CarouselSlider />
+      <CarouselSlider products={products} />
       <div className="text-4xl text-center font-bold mt-12 mb-12 border-b-2 border-yellow-400 py-2">Hogy miért vásárolj a Zümin?</div>
       <Qualities />
-      <AboutSection/>
+      <AboutSection />
     </div>
   );
 }
